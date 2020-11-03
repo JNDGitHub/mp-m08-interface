43fed747ace7e21a29657619c953c8ade4005962
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-11-03 10:13:28 -0300

    Integração com o report
    A função ReportImage foi integrada

--- project/main.py
+++ project/main.py
@@ -2,8 +2,9 @@ from flask import Blueprint, render_template, request, jsonify, render_template_
 from . import db
 from . import dialog
 from flask_login import login_required, current_user
-from report import Report
-import os
+from report import ReportImage
+from log import Log
+import os, codecs
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -45,12 +46,21 @@ def new_analysis():
 @login_required
 def search_analysis():
     return render_template('search_analysis.html')
+
 @main.route('/report', methods=['POST', 'GET'])
 @login_required
 def report():
-    log_path = os.getcwd()
-    with open('./project/teste_header.html', 'r') as f:
+    logPath = os.getcwd()
+    logObj = Log()
+    with codecs.open('./report_header.html', encoding = 'utf-8', mode = 'r') as f:
         header = f.read()
-    img_report = Report(log_path, './project/imagens_manuais4.npz') 
-    conteudo = img_report.generate_img(return_path=False)
-    return render_template_string(header+conteudo+'{% endblock %}', id_tabela = '#T_f0a9eaf6_1199_11eb_85a2_7429afa56915', name=current_user.name, id_report="T_f0a9eaf6_1199_11eb_85a2_7429afa56915", path=log_path)
+
+    img_report = ReportImage(logPath, './project/imagens_manuais4', logObj) 
+    conteudo = img_report.generate_report(return_path=False)
+    return render_template_string(header + conteudo[0] + '{% endblock %}', id_tabela = conteudo[1], name=current_user.name, id_report="T_f0a9eaf6_1199_11eb_85a2_7429afa56915", path=logPath)
+
+@main.route('/teste', methods=['POST', 'GET'])
+@login_required
+def teste():
+    idd = "#banana"
+    return render_template('teste.html', id=idd)
